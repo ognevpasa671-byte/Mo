9f9d71586ce01f05d33fde69b1fca87ef3a59530
ognevpasa671-byte <ognevpasa671@gmail.com>
2025-11-06 12:55:30 +0300

    Add files via upload

---
+++ prac1/Functions/zad5.js
@@ -0,0 +1,8 @@
+function firstUniqueChar(str) {
+  for (let char of str) {
+    if (str.indexOf(char) === str.lastIndexOf(char)) return char;
+  }
+  return null;
+}
+
+console.log(firstUniqueChar('абракадабра'));
