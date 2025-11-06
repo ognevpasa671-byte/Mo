f483a62b896a68bcf2087c8f3d8d3afbf6a9ae08
ognevpasa671-byte <ognevpasa671@gmail.com>
2025-11-06 12:55:00 +0300

    Delete prac1/Arrays/Functions directory

--- prac1/Arrays/Functions/zad5.js
+++
@@ -1,8 +0,0 @@
-function firstUniqueChar(str) {
-  for (let char of str) {
-    if (str.indexOf(char) === str.lastIndexOf(char)) return char;
-  }
-  return null;
-}
-
-console.log(firstUniqueChar('абракадабра'));
