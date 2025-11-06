b3ebd47d1fbb8db4f19adb98710868b3413c2892
ognevpasa671-byte <ognevpasa671@gmail.com>
2025-11-06 12:49:05 +0300

    Add files via upload

---
+++ prac1/Variables/zad2.js
@@ -0,0 +1,7 @@
+for (var a = 1; a < 2; a++) {}
+//for (let b = 1; b < 2; b++) {}
+//for (const c = 1; c < 2; c++) {}
+
+console.log(a); // видно
+//console.log(b); // ReferenceError
+//console.log(c); // ReferenceError
