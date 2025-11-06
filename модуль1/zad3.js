ffe19e6013ab0b9431aadba4af85b9cc20f68256
ognevpasa671-byte <ognevpasa671@gmail.com>
2025-11-06 12:11:48 +0300

    Add files via upload

---
+++ модуль1/zad3.js
@@ -0,0 +1,17 @@
+function firstUniqueChar(str) {
+  const charCount = {};
+
+  for (const char of str) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+  for (const char of str) {
+    if (charCount[char] === 1) {
+      return char;
+    }
+  }
+
+  return null; 
+}
+
+console.log(firstUniqueChar("абракадабра")); 
